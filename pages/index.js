14410c573a812f422f3205c7633c4a3b42265e41
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 20:27:38 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -409,6 +409,13 @@ export default function Home() {
             className="newsletter__input"
           />
 <ContactForm />
+<form action="/send-data-here" method="post">
+  <label for="first">First name:</label>
+  <input type="text" id="first" name="first" />
+  <label for="last">Last name:</label>
+  <input type="text" id="last" name="last" />
+  <button type="submit">Submit</button>
+</form>
           <a href="#" className="book--now">
             <Image
               src="images/svg/booking.svg"
