d59ea54788e5e2e93c4668d0aceaa4a26a9d9a9f
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 13:28:15 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -19,11 +19,11 @@ function HeaderComponent() {
      
 
  </Head>
-       </>
+       
   <Script strategy="lazyOnload" src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.2/swiper-bundle.min.js'></Script>
 <Script strategy="lazyOnload" src='https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.min.js'></Script>
 <Script strategy="lazyOnload" src="./script.js"></Script>
-      
+      </>
     )
 }
 export default HeaderComponent
