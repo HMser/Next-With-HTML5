ee33e4d7983463e9fa8b3413ce2fea4881687057
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 13:41:16 +0530

    Update Hello.js

--- pages/Hello.js
+++ pages/Hello.js
@@ -1,4 +1,4 @@
-import HeaderComponent from "../components/header";
+import HeaderComponent from "../components/navbar";
 import FooterComponent from "../components/footer";
 
 function HelloPage(){    
