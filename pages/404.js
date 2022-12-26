b6eacd6b504d1b2b1ce8d3030cd5abcb50871755
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 13:11:42 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,9 @@
 // 404.js
 import Link from 'next/link'
-
+import SpotifyFooter from "../components/SpotifyFooter";
 export default function FourOhFour() {
   return <>
+    <SpotifyFooter />
     <h1>404 - Page Not Found</h1>
     <Link href="/">
       <a>
