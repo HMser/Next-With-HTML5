58129e86abf2aec46185a2a0d5d4d61302ea37dd
HM <95879255+HMser@users.noreply.github.com>
2022-12-27 00:43:06 +0530

    Create [id].js

---
+++ pages/posts/[id].js
@@ -0,0 +1,10 @@
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+
+  return {
+    props: {
+      postData,
+    },
+  };
+}
