7ee8e8541994a27a6bdd46c9de69ed44df09ed63
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 13:24:53 +0530

    Update header.js

--- components/header.js
+++ components/header.js
@@ -1,6 +1,28 @@
 function HeaderComponent() {    
     return (        
-        <div>I am header component</div>    
+         <Head>
+          <meta charset="UTF-8" />
+<meta name="google" content="notranslate" />
+<link rel="apple-touch-icon" sizes="180x180" href="icons/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="icons/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="icons/favicon-16x16.png" />
+<link rel="manifest" href="icons/site.webmanifest" />
+<link rel="mask-icon" href="icons/safari-pinned-tab.svg" color="#5bbad5" />
+<meta name="msapplication-TileColor" content="#2d89ef" />
+<meta name="theme-color" content="#ffffff" />
+
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+  <title>StatusArea</title>
+  <link rel='stylesheet' href='https://assets.codepen.io/7773162/swiper-bundle.min.css' />
+<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css '/>
+     
+
+ </Head>
+
+  <Script strategy="lazyOnload" src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.2/swiper-bundle.min.js'></Script>
+<Script strategy="lazyOnload" src='https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.min.js'></Script>
+<Script strategy="lazyOnload" src="./script.js"></Script>
+      
     )
 }
 export default HeaderComponent
