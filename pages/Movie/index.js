0431dc4b2e62d4adb7ef79f831efc52dfaa0caa8
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 22:52:46 +0530

    Create index.js

---
+++ pages/Movie/index.js
@@ -0,0 +1,18 @@
+import HeaderComponent from "../components/navbar";
+import FooterComponent from "../components/footer";
+
+function MoviePage(){    
+    return(        
+        <div>            
+        <HeaderComponent></HeaderComponent>            
+        <br />            
+        <div>                
+        	Hello there, How are you?            
+        </div>                
+        <br />            
+        <FooterComponent></FooterComponent>        
+       </div>    
+    )
+}
+
+export default MoviePage
