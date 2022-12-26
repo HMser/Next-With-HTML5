029bf285c02d87813bdb25c61efeeaa0b6b85509
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 12:00:53 +0530

    Update contactForm.js

--- components/contactForm.js
+++ components/contactForm.js
@@ -1,25 +1,45 @@
-import { useForm, ValidationError } from "@formspree/react";
-
-export default function ContactForm() {
-  const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORM);
-
+// Make sure to run npm install @formspree/react
+// For more help visit https://formspr.ee/react-help
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
+function ContactForm() {
+  const [state, handleSubmit] = useForm("xeqwjnqp");
   if (state.succeeded) {
-    return <p>Thanks for your submission!</p>;
+      return <p>Thanks for joining!</p>;
   }
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email Address</label>
-      <input id="email" type="email" name="email" />
-      <ValidationError prefix="Email" field="email" errors={state.errors} />
-
-      <textarea id="message" name="message" />
-      <ValidationError prefix="Message" field="message" errors={state.errors} />
-
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
       <button type="submit" disabled={state.submitting}>
         Submit
       </button>
-      <ValidationError errors={state.errors} />
     </form>
   );
 }
+function App() {
+  return (
+    <ContactForm />
+  );
+}
+export default App;
