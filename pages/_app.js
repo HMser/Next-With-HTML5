1062f7e804baaabe75e131731239ee4b27e40b6f
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 14:20:30 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import '../styles/globals.css'
-
+import Script from 'next/script'
 // pages/_app.js
 
 import Layout from '../components/layout'
 
 export default function MyApp({ Component, pageProps }) {
   return (
+<Script strategy="lazyOnload" src='https://cdnjs.cloudflare.com/ajax/libs/Swiper/8.4.2/swiper-bundle.min.js'></Script>
+<Script strategy="lazyOnload" src='https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.9/scrollreveal.min.js'></Script>
+<Script strategy="lazyOnload" src="./script.js"></Script>
     <Layout>
       <Component {...pageProps} />
     </Layout>
