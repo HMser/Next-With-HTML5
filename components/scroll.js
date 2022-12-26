bdce0a7c40b5cb7549db87c53d983c6a494e3c51
HM <95879255+HMser@users.noreply.github.com>
2022-12-26 14:28:43 +0530

    Create scroll.js

---
+++ components/scroll.js
@@ -0,0 +1,12 @@
+function ScrollComponent() {    
+	return (        
+		<>
+{/* SCROLL UP */}
+  <a href="#" className="scrollup" id="scroll-up">
+    <i className="bx bx-up-arrow-alt scrollup__icon" />
+  </a>
+  
+               </>
+	)
+}
+export default ScrollComponent
