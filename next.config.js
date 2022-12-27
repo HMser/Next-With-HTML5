1fa105abb8fc77161290e5e415e665235814488d
HM <95879255+HMser@users.noreply.github.com>
2022-12-27 17:59:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-   pageExtensions: ['[id]''js', 'jsx', 'md', 'mdx'],
+   pageExtensions: ['[id]','js', 'jsx', 'md', 'mdx'],
 }
 
 module.exports = nextConfig
