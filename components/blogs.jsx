b9b58a537ddc8049efc07eaacc3d5b9560d9344a
HM <95879255+HMser@users.noreply.github.com>
2022-12-27 18:39:34 +0530

    Update blogs.jsx

--- components/blogs.jsx
+++ components/blogs.jsx
@@ -1,3 +1,8 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Script from 'next/script'
+import Link from 'next/link'
+
     {siteBlogs.map((post) => (
      <Link key={post.id} href={`/blogs/${post.slug}`}>
         <a>
